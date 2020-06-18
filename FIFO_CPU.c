81abf37f8f1a02b9683bc75ab3ac9ac2900ba14f
MayankB02 <55989344+MayankB02@users.noreply.github.com>
2020-06-18 22:30:18 +0530

    Update FIFO_CPU.c
    Arrival TIme added

--- FIFO_CPU.c
+++ FIFO_CPU.c
@@ -4,80 +4,101 @@
 struct process 
 {
     int pid ;
+    int at ;
     int bt ; 
     int wt ;
+    int ct ;
     int tat ;
 };
 
 int findWaitingTime(struct process *A , int n)
 {
-    A[0].wt = 0 ;
     int sum = 0 ;
-    for(int i = 1 ; i < n ; i++)
+    for(int i = 0 ; i < n ; i++)
     {
-        A[i].wt = A[i-1].wt + A[i-1].bt;
+        A[i].wt = A[i].tat - A[i].bt ;
         sum = sum + A[i].wt;
     }
     return sum;
+}
+void findCompletionTime(struct process *A , int n)
+{
+    A[0].ct = A[0].bt;
+    for(int i = 1 ; i < n ; i++)
+    {
+        int adjust = 0 ;
+        if(A[i-1].ct < A[i].at)
+        {
+           A[i].ct = A[i].at + A[i].bt;
+           continue;
+        }
+        A[i].ct = A[i-1].ct + A[i].bt;
+    }
 }
 int findTurnAroundTime(struct process *A , int n)
 {
     int sum = 0;
     for(int i = 0 ; i < n ; i++)
     {
-        A[i].tat = A[i].wt + A[i].bt;
+        A[i].tat = A[i].ct - A[i].at;
         sum = sum + A[i].tat;
     }
     return sum;
 }
-void display(struct process *A , int n)
+
+void sort_by_ArrivalTime(struct process *A , int n)
 {
-    printf("\nProcess ID\tBurst Time\tTurnaround Time\tWaiting Time");
-    for(int i = 0 ; i<n ; i++)
-        printf("\n%d\t\t%d\t\t%d\t\t%d" , A[i].pid,  A[i].bt , A[i].tat , A[i].wt);
+    for(int i = 0 ; i < n ; i++)
+    {
+        int pos = i ;
+        for(int j = i+1 ; j < n ; j++)
+        {
+            if(A[pos].at > A[j].at)
+                pos = j ;
+        }
+        struct process v = A[pos];
+        A[pos] = A[i];
+        A[i] = v;
+    }
 }
+
 void findAvgTime(struct process *A , int n)
 {
-    int tot_wt = findWaitingTime(A,n);
+    findCompletionTime(A ,n);
     int tot_tat = findTurnAroundTime(A,n);
+    int tot_wt = findWaitingTime(A,n);
     printf("\nAverage waiting time : %f" , (float)tot_wt/n );
     printf("\nAverage turnaround time : %f" , (float)tot_tat/n );
 }
 
+void display(struct process *A , int n)
+{
+    printf("\nProcess ID\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time");
+    for(int i = 0 ; i<n ; i++)
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d" , A[i].pid, A[i].at , A[i].bt , A[i].ct , A[i].tat , A[i].wt);
+}
 int main()
 {
-    int n = 0 ;
-    //Input
-    printf("\nEnter the number of processes : ");
-    scanf("%d" , &n);
-    struct process proc[n] ;
-    for(int i = 0 ; i<n ; i++)
-    {
-        printf("\nEnter the burst time for process id %d: " , (i+1));
-        proc[i].pid = i+1;
-        scanf("%d" , &proc[i].bt);
-        proc[i].wt=0;
-        proc[i].tat = 0;
-    }
-    //struct process proc[] = {{1,7,0,0},{2,5,0,0},{3,1,0,0},{4,2,0,0},{5,8,0,0}};
-    //int n = sizeof proc / sizeof proc[0];
+    //int n = 0 ;
+    // printf("\nEnter the number of processes : ");
+    // scanf("%d" , &n);
+    // struct process proc[n] ;
+    // for(int i = 0 ; i<n ; i++)
+    // {
+    //     printf("\nEnter the arrival time and  burst time for process id %d: " , (i+1));
+    //     proc[i].pid = i+1;
+    //     scanf("%d %d" , &proc[i].at , &proc[i].bt);
+    //     proc[i].wt=0;
+    //     proc[i].ct = 0 ;
+    //     proc[i].tat = 0;
+    // }
+    struct process proc[] = {{1,0,4,0,0,0},{2,1,3,0,0,0},{3,2,1,0,0,0},{4,3,2,0,0,0},{5,11,5,0,0,0}};
+    int n = sizeof proc / sizeof proc[0];
     printf("\nInitial State");
     display(proc,n);
+    sort_by_ArrivalTime(proc,n);
     printf("\nAfter execution");
     findAvgTime(proc,n);
     display(proc,n);    
 }
   
-    
-
-    
-    
-
-
-
-
-    
-
-
-
-
